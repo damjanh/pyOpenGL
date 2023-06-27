cf166010f65cbfeeb331df86df606764ebfb20b0
Damjan Hudobreznik <damjan.hudobreznik@gmail.com>
2023-06-27 12:48:57 +0200

    Add vertex and fragment shader, and render triangle.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame as pg
 from OpenGL.GL import *
+import numpy as np
+from OpenGL.GL.shaders import compileProgram, compileShader
 
 
 class App:
@@ -10,7 +12,23 @@ class App:
 
         # Initialise OpenGl
         glClearColor(0.1, 0.2, 0.2, 1)
+        self.shader = self.createShader('shaders/vertex.txt', 'shaders/fragment.txt')
+        glUseProgram(self.shader)
+        self.triangle = Triangle()
         self.main_loop()
+
+    def createShader(self, vertex_filepath, fragment_filepath):
+        with open(vertex_filepath, 'r') as f:
+            vertex_src = f.readlines()
+
+        with open(fragment_filepath, 'r') as f:
+            fragment_src = f.readlines()
+
+        shader = compileProgram(
+            compileShader(vertex_src, GL_VERTEX_SHADER),
+            compileShader(fragment_src, GL_FRAGMENT_SHADER)
+        )
+        return shader
 
     def main_loop(self):
         running = True
@@ -20,6 +38,12 @@ class App:
                     running = False
 
             # Refresh screen
+            glClear(GL_COLOR_BUFFER_BIT)
+
+            glUseProgram(self.shader)
+            glBindVertexArray(self.triangle.vao)
+            glDrawArrays(GL_TRIANGLES, 0, self.triangle.vertex_count)
+
             pg.display.flip()
 
             self.clock.tick(60)
@@ -27,7 +51,37 @@ class App:
         self.quit()
 
     def quit(self):
+        self.triangle.destroy()
+        glDeleteProgram(self.shader)
         pg.quit()
+
+
+class Triangle:
+    def __init__(self):
+        # x, y, z, r, g, b
+        self.vertices = (
+            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
+            0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+            0.0, 0.5, 0.0, 0.0, 0.0, 1.0
+        )
+
+        self.vertices = np.array(self.vertices, dtype=np.float32)
+
+        self.vertex_count = 3
+
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+
+    def destroy(self):
+        glDeleteVertexArrays(1, (self.vao,))
+        glDeleteBuffers(1, (self.vbo,))
 
 
 if __name__ == '__main__':
