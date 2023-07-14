d1c328a013129ea630d0c4ccc1a3b02e59afa8a3
Damjan Hudobreznik <damjan.hudobreznik@gmail.com>
2023-07-14 10:32:56 +0200

    Separate material into its own file.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from OpenGL.GL import glDeleteTextures
 from OpenGL.GL.shaders import compileProgram, compileShader
 import pyrr
+from material import Material
 
 
 class Cube:
@@ -274,30 +275,6 @@ class CubeMesh:
     def destroy(self):
         glDeleteVertexArrays(1, (self.vao,))
         glDeleteBuffers(1, (self.vbo,))
-
-
-class Material:
-    def __init__(self, filepath):
-        self.texture = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_2D, self.texture)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-
-        image = pg.image.load(filepath).convert_alpha()
-        image_width, image_height = image.get_rect().size
-        image_data = pg.image.tostring(image, 'RGBA')
-
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
-        glGenerateMipmap(GL_TEXTURE_2D)
-
-    def use(self):
-        glActiveTexture(GL_TEXTURE0)
-        glBindTexture(GL_TEXTURE_2D, self.texture)
-
-    def destroy(self):
-        glDeleteTextures(1, (self.texture, ))
 
 
 if __name__ == '__main__':
